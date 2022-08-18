86167365cc46ed62d24911e4e053c5e0852a7dcd
Daniel Dansie <dan.dansie126@gmail.com>
2022-08-18 09:05:25 -0600

    added code to files and added js file

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color', 'white');
+
+$('#nav').css('background-color', '#145');
+
+$('li:nth-of-type(even)').css('background-color', '#ccc');
+
+$('li:nth-of-type(odd)').css('background-color', '#aaa');
+
+$('input').value('Daniel');
